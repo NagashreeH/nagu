6a4d80978e16e2233d0a810483d57b959ef5b8f3
USER <USER@dell>
2019-04-24 19:10:38 +0530

    commit1

---
+++ Demotest/src/test/java/ChromeTest.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ChromeTest {
+@Test
+public void ChromeTest() {                
+ChromeDriver driver=new ChromeDriver();
+}
+}
